783a0e769411ba3d82d18c34c93469a0ec700cf1
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2017-12-10 08:12:22 -0500

    AoC 2017 - Day 8 - R

---
+++ 2017/day08-r/day08b.r
@@ -0,0 +1,90 @@
+library(hash)
+
+df <- read.delim("day08.input", sep=" ")
+
+m<-as.matrix(df)
+
+h <- hash() 
+maximum<-0
+
+#register op value iff register2 comp value2
+con = file("day08.input", "r")
+while ( TRUE ) {
+    line = readLines(con, n = 1)
+    if ( length(line) == 0 ) {
+        break
+    }
+    el<-strsplit(line, " ")[[1]]
+    #print(cat(el[0])
+    #cat(el[2],el[1])
+    #print("")
+
+
+    if (!(el[1]  %in% keys(h))) {
+        h[el[1]]=0
+    }
+
+    if (!(el[5]  %in% keys(h))) {
+        h[el[5]]=0
+    }    
+    
+    cond<-FALSE
+
+    reg<-el[5]
+    com<-el[6]
+    val<-el[7]
+    val<-as.numeric(as.character(val))
+
+    #sprintf("reg: %s, val: %s, com: %s", reg, val, com)
+    #print("reg val cm")
+    #print(reg)
+    #print(val)
+    #print(com)
+
+    if (com==">" && (h[[reg]]>as.integer(val))) {
+        cond<-TRUE
+    }
+    if (com=="<" &&(h[[reg]]<val)) {
+        cond<-TRUE
+    }
+    if (com=="==" &&(h[[reg]]==val)) {
+        cond<-TRUE
+    }
+    if (com==">="&& (h[[reg]]>=val)) {
+        cond<-TRUE
+    }
+    if (com=="<=" &&(h[[reg]]<=val)) {
+        cond<-TRUE
+    }
+    if (com=="!="&&(h[[reg]]!=val)) {
+        cond<-TRUE
+    }
+    reg<-el[1]
+    ops<-el[2]
+    val<-el[3]
+    val<-as.numeric(as.character(val))
+
+
+    if (cond==TRUE) {
+        if (ops =="dec"){
+            #print(class(val))
+            #print(val)
+            #print(">>>>")
+            h[reg]<-h[[reg]]-val
+        }
+        else
+        {
+            h[reg]<-h[[reg]]+val
+        }
+    }
+
+
+    if( maximum<values(h)[[which.max(values(h))]]){
+        maximum<-values(h)[[which.max(values(h))]]
+    }
+
+}
+close(con)
+
+
+print(maximum)
