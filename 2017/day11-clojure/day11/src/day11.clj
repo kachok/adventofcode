cccf69305c62b06b79198aec6d24e67c45fe2b01
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2017-12-12 11:57:35 -0500

    AoC 2017 - Day 11 - Clojure

---
+++ 2017/day11-clojure/day11/src/day11.clj
@@ -0,0 +1,68 @@
+(ns day11
+  (:require [clojure.string :as str]))
+
+
+
+(defn abs "(abs n) is the absolute value of n" [n]
+(cond
+(not (number? n)) (throw (IllegalArgumentException.
+                            "abs requires a number"))
+(neg? n) (- n)
+:else n))
+
+
+(defn calc [line]
+
+    (require '[clojure.string :as str])
+
+    (str/split line #",")
+
+    (def f (frequencies (str/split line #",")))
+
+    (def s (or (get f "s") 0))
+    (def n (or (get f "n") 0))
+    (def se (or (get f "se") 0))
+    (def sw (or (get f "sw") 0))
+    (def ne (or (get f "ne") 0))
+    (def nw (or (get f "nw") 0))
+
+    ;;{"s" 1342, "nw" 1071, "sw" 1372, "ne" 1628, "se" 1511, "n" 1299}
+
+    ;;{"s" 1342, "nw" 1071, "sw" 1372, "ne" 1628, "se" 1511, "n" 1299}
+    ;;"s" 43 "se" 440 "ne" 256
+    ;;"se" 440+43 "ne" 256-43
+    ;;"se" 483 "ne" 213
+    ;;696
+
+    ;;a0=0
+    ;;b0=0
+
+    ;;a1=s-n -(ne+nw)= 
+    ;;b1=ne+se-sw-nw=(ne + se) - (sw + nw)
+
+
+    (def x (- (+ n ne) (+ s sw)))
+    (def y (- (+ se ne) (+ nw sw)))
+
+    ;;if( condition
+    ;;value-if-true
+    ;;value-if-false)
+
+    (defn not-negative? [n1 n2]
+        (if (<= 0 (* x y)) true false))
+
+    (def dist (if(not-negative? x y) (max (abs x) (abs y)) (+ (abs x) (abs y)))) 
+
+    dist
+    )
+
+
+
+(defn -main []
+
+    (def line (slurp "day11.input"))
+    ;;(def line "ne,ne,ne")
+    (def dist (calc line))
+    (println "answer is: " dist))
+
+
